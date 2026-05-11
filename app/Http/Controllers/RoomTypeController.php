b8e07693c9b3e8afe93c92ab3c6a1bfbb378898c
rani <husnulsyukur6@gmail.com>
2026-05-11 09:51:40 +0800

    update model

---
+++ app/Http/Controllers/RoomTypeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RoomType;
+use Illuminate\Http\Request;
+
+class RoomTypeController extends Controller
+{
+    public function index()
+    {
+        return response()->json(RoomType::all());
+    }
+
+    public function store(Request $request)
+    {
+        $roomType = RoomType::create($request->all());
+
+        return response()->json($roomType, 201);
+    }
+
+    public function show(string $id)
+    {
+        return response()->json(RoomType::findOrFail($id));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $roomType = RoomType::findOrFail($id);
+
+        $roomType->update($request->all());
+
+        return response()->json($roomType);
+    }
+
+    public function destroy(string $id)
+    {
+        RoomType::destroy($id);
+
+        return response()->json([
+            'message' => 'Data berhasil dihapus'
+        ]);
+    }
+}
